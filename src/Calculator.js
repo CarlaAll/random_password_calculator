23cd92629d6902b79571dfbe7fc630a2159fae93
CarlaAll <69358793+CarlaAll@users.noreply.github.com>
2020-09-17 20:29:07 +0200

    framework of calculator
    - frame HTML + CSS

---
+++ src/Calculator.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Calculator.css";
+
+export default function Calculator() {
+  return (
+    <div className="Calculator">
+      <p>PLACEHOLDER PW</p>
+      <button className="btn btn-primary "> New Password</button>
+    </div>
+  );
+}
