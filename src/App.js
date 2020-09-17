3cc6e625e67f37725684e669ec16488b99e2292b
CarlaAll <69358793+CarlaAll@users.noreply.github.com>
2020-09-17 20:36:42 +0200

    footer added incl. Open-Source link and hosting-link

--- src/App.js
+++ src/App.js
@@ -4,9 +4,29 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Random Password Generator</h1>
-      <Calculator />
+    <div>
+      <div className="App">
+        <h1>Random Password Generator</h1>
+        <Calculator />
+      </div>
+      <footer>
+        Open-Source by{" "}
+        <a
+          href="https://github.com/CarlaAll/random_password_calculator"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Carla Allenbach
+        </a>{" "}
+        and is hosted on{" "}
+        <a
+          href="https://generate-random-pw.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Netlify
+        </a>
+      </footer>
     </div>
   );
 }
